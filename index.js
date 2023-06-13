f667c31921e885063f1dfc8487d6e3fd6ab534c5
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-06-13 14:08:14 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//import mysql library
+import mysql from 'mysql2';
+import connectionInfo from './secretStuff.js';
+
+//then we connect to (our) mysql database (have to give it our credentials)
+const connection = mysql.createConnection(connectionInfo);
+
+// runs simple query to get 2 movies
+connection.query('SELECT * FROM movietable',
+function (error, results){
+    console.log(error); 
+    //console.table the results
+    console.table(results);
+    // connection.destroy();
+});
+
+
